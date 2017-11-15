b8170a1a9eaf1024c6ca16cc89416333e4ea59e1
UDARA BIBILE <chathuranga94@live.com>
2017-11-14 21:31:33 -0800

    hadoop code done

---
+++ AvgById.java
@@ -0,0 +1,73 @@
+
+package bibi;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+public class AvgById {
+public static void main(String [] args) throws Exception
+{
+Configuration c=new Configuration();
+String[] files=new GenericOptionsParser(c,args).getRemainingArgs();
+Path input=new Path(files[0]);
+Path output=new Path(files[1]);
+Job j=new Job(c,"average");
+j.setJarByClass(AvgById.class);
+
+j.setMapperClass(MapForAvgById.class);
+j.setReducerClass(ReduceForAvgById.class);
+
+j.setOutputKeyClass(Text.class);
+j.setOutputValueClass(FloatWritable.class);
+
+FileInputFormat.addInputPath(j, input);
+FileOutputFormat.setOutputPath(j, output);
+System.exit(j.waitForCompletion(true)?0:1);
+}
+public static class MapForAvgById extends Mapper<LongWritable, Text, Text, FloatWritable>{
+public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
+{
+	String line = value.toString();
+	String[] words=line.split(",");
+
+	System.out.println(words[0] + " >>> " + words[2]);
+	Text outputKey_INDEX = new Text(words[0]);
+	FloatWritable outputValue_GPA = new FloatWritable( Float.parseFloat(words[2]) );
+
+	con.write(outputKey_INDEX, outputValue_GPA);
+}
+}
+
+public static class ReduceForAvgById extends Reducer<Text, FloatWritable, Text, FloatWritable>
+{
+public void reduce(Text word, Iterable<FloatWritable> values, Context con) throws IOException, InterruptedException
+{
+	//FloatWritable sum = new FloatWritable(0);
+
+	float sum = 0;	
+	int count = 0;	
+
+	for(FloatWritable value : values)
+   	{
+		sum += Float.parseFloat(value.toString());
+		count += 1;
+   	}
+
+	float avg = sum / count;
+	con.write(word, new FloatWritable(avg));
+	
+	System.out.println(word + "	" + avg);
+}
+}
+
+}
---
+++ hadoop.sh
@@ -0,0 +1,36 @@
+echo "RUNNING HADOOP.SH"
+export HADOOP_HOME=/usr/local/hadoop
+
+echo "cleanup previous input_dir..."
+$HADOOP_HOME/bin/hadoop fs -rm -r input_dir 
+printf "\n"
+
+echo "make input_dir..."
+$HADOOP_HOME/bin/hadoop fs -mkdir input_dir 
+printf "\n"
+
+echo "add csv file to input_dir..."
+$HADOOP_HOME/bin/hadoop fs -put /home/bibi/Desktop/HADOOP/distributed.csv input_dir 
+printf "\n"
+
+echo "check files in input_dir..."
+$HADOOP_HOME/bin/hadoop fs -ls input_dir/
+printf "\n\n"
+
+echo "remove existing compiled folder..."
+rm -r compiled/ && mkdir compiled
+
+echo "remove existing output folder..."
+rm -r output_dir
+
+echo "compile jar file"
+javac -classpath hadoop-core-1.2.1.jar:commons-cli-1.2.jar -d compiled AvgById.java
+jar -cvf avg.jar -C compiled/ .
+printf "\n\n\n"
+
+$HADOOP_HOME/bin/hadoop jar avg.jar bibi.AvgById input_dir/distributed.csv output_dir
+
+printf "\n"
+$HADOOP_HOME/bin/hadoop fs -ls output_dir
+printf "\n"
+$HADOOP_HOME/bin/hadoop fs -cat output_dir/part-r-00000
